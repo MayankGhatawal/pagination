da622a138975b95a34683d4b2cd371386e97a0e8
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-09-08 17:16:45 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
+import './App.css';
+import { useEffect } from 'react';
 
 function App() {
-  
+  const fetchproducts = async () => {
+    const res = await fetch('https://fakestoreapi.com/products');
+    const data = res.json();
+    console.log(data);
+  };
 
+  useEffect(() => {
+    fetchproducts();
+  }, []);
   return (
     <>
-      
+      <div className="main"></div>
     </>
   )
 }
